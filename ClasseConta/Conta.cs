5f92276f74df6161407a8a45ec98dbc0a3de50f3
Guilherme <guilhermerafaell10@gmail.com>
2026-08-10 20:05:51 -0300

    Declaração de atributos e métodos da classe

---
+++ ClasseConta/Conta.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Pasta
+namespace ClasseConta
+{
+    public class Conta
+    {
+        //Declaração de atributos
+        int numero;
+        string titular;
+        double saldo;
+
+        //Declaração dos métodos(funções)
+        void Sacar()
+        {
+
+        }
+
+        void Depositar()
+        {
+            
+        }
+    }
+}
